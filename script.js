99c7a1714de5597d219505f15d7226dada47f6b6
Rony2518 <113433588+Rony2518@users.noreply.github.com>
2023-03-25 21:02:33 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,116 @@
+// Cache the DOM elements
+const userScore_span = document.getElementById("user-score");
+const computerScore_span = document.getElementById("computer-score");
+const scoreBoard_div = document.querySelector(".scoreboard");
+const result_p = document.getElementById("result");
+const rock_div = document.getElementById("rock");
+const paper_div = document.getElementById("paper");
+const scissors_div = document.getElementById("scissors");
+const reset_button = document.getElementById("reset-button");
+
+// Set initial scores to 0
+let userScore = 0;
+let computerScore = 0;
+
+// Reset game
+function resetGame() {
+    userScore = 0;
+    computerScore = 0;
+    userScore_span.innerHTML = userScore;
+    computerScore_span.innerHTML = computerScore;
+    result_p.innerHTML = "";
+}
+
+// Get computer's choice
+function getComputerChoice() {
+    const choices = ["rock", "paper", "scissors"];
+    const randomNumber = Math.floor(Math.random() * 3);
+    return choices[randomNumber];
+}
+
+// Convert the choice to readable format
+function convertToWord(choice) {
+    if (choice === "rock") return "Rock";
+    if (choice === "paper") return "Paper";
+    return "Scissors";
+}
+
+// Win function
+function win(userChoice, computerChoice) {
+    userScore++;
+    userScore_span.innerHTML = userScore;
+    computerScore_span.innerHTML = computerScore;
+    result_p.innerHTML = `${convertToWord(userChoice)} beats ${convertToWord(computerChoice)}. You win!`;
+    document.getElementById(userChoice).classList.add('green-glow');
+    setTimeout(() => document.getElementById(userChoice).classList.remove('green-glow'), 500);
+}
+
+// Lose function
+function lose(userChoice, computerChoice) {
+    computerScore++;
+    userScore_span.innerHTML = userScore;
+    computerScore_span.innerHTML = computerScore;
+    result_p.innerHTML = `${convertToWord(computerChoice)} beats ${convertToWord(userChoice)}. You lose...`;
+    document.getElementById(userChoice).classList.add('red-glow');
+    setTimeout(() => document.getElementById(userChoice).classList.remove('red-glow'), 500);
+}
+
+// Draw function
+function draw(userChoice, computerChoice) {
+    userScore_span.innerHTML = userScore;
+    computerScore_span.innerHTML = computerScore;
+    result_p.innerHTML = `It's a draw.`;
+    document.getElementById(userChoice).classList.add('gray-glow');
+    setTimeout(() => document.getElementById(userChoice).classList.remove('gray-glow'), 500);
+}
+
+// Main game function
+function game(userChoice) {
+    const computerChoice = getComputerChoice();
+
+    switch (userChoice + computerChoice) {
+        case "rockscissors":
+        case "paperrock":
+        case "scissorspaper":
+            win(userChoice, computerChoice);
+            break;
+        case "rockpaper":
+        case "paperscissors":
+        case "scissorsrock":
+            lose(userChoice, computerChoice);
+            break;
+        case "rockrock":
+        case "paperpaper":
+        case "scissorsscissors":
+            draw(userChoice, computerChoice);
+            break;
+    }
+    if (userScore === 10) {
+        setTimeout(() => {
+            result_p.innerHTML = "Congratulations! You won the game!";
+            resetGame()
+        }, 1000);
+    } else if (computerScore === 10) {
+        setTimeout(() => {
+            result_p.innerHTML = "Sorry, you lost the game.";
+            resetGame()
+        }, 1000);
+    }
+}
+
+// Add event listeners
+rock_div.addEventListener("click", function () {
+    game("rock");
+})
+
+paper_div.addEventListener("click", function () {
+    game("paper");
+})
+
+scissors_div.addEventListener("click", function () {
+    game("scissors");
+})
+
+reset_button.addEventListener("click", function () {
+    resetGame();
+});
